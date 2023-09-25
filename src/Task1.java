7eb73144d5a8d816fe78630bd56e54a8b3e71554
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-09-25 19:31:56 +0200

    init commit

---
+++ src/Task1.java
@@ -0,0 +1,6 @@
+//Задача 1: Сравнение эффективности линейного и бинарного поиска
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
